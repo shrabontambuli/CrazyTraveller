e6b45063cd880f4e721d21dfba3fae38776c9210
ShrabonTambuli <shrabonsa@gmail.com>
2023-11-04 00:30:24 +0600

    code changes

--- src/Components/Home/TourPackages/TourPackages.jsx
+++ src/Components/Home/TourPackages/TourPackages.jsx
@@ -9,36 +9,36 @@ const TourPackages = () => {
                 <p className='w-9/12 mx-auto mt-6'>A package tour, package vacation, or package holiday comprises transport and accommodation advertised and sold together by a vendor known as a tour operator. Other services may be provided such as a rental car, activities or outings during the holiday.</p>
             </div>
             <div className='mt-20 grid grid-cols-1 lg:grid-cols-3 justify-items-center gap-8 container mx-auto'>
-                <div className='div2 w-11/12'>
+                <div className='div2 w-11/12 pb-4'>
                     <img className='w-full h-96' src="/images/rome.jpg" alt="" />
                     <div className='flex justify-between items-center px-5 mt-8'>
                         <div>
                             <h1 className='text-black text-4xl font-serif'>Rome, Italy</h1>
-                            <p className='text-[#1E1E1E]'>10 days trip</p>
+                            <p className='text-[#1E1E1E] mt-2'>10 days trip</p>
                         </div>
                         <div>
                             <p className='font-semibold'>$5.24K</p>
                         </div>
                     </div>
                 </div>
-                <div className='div2 w-11/12'>
+                <div className='div2 w-11/12 pb-4'>
                     <img className='w-full h-96' src="/images/lahore.jpg" alt="" />
                     <div className='flex justify-between items-center px-5 mt-8'>
                         <div>
-                            <h1 className='text-black text-4xl font-serif'>Lahore, Lakistan</h1>
-                            <p className='text-[#1E1E1E]'>6 days trip</p>
+                            <h1 className='text-black text-4xl font-serif'>Lahore</h1>
+                            <p className='text-[#1E1E1E] mt-2'>6 days trip</p>
                         </div>
                         <div>
                             <p className='font-semibold'>$3.25K</p>
                         </div>
                     </div>
                 </div>
-                <div className='div2 w-11/12'>
+                <div className='div2 w-11/12 pb-4'>
                     <img className='w-full h-96' src="/images/singapore2.jpg" alt="" />
                     <div className='flex justify-between items-center px-5 mt-8'>
                         <div>
                             <h1 className='text-black text-4xl font-serif'>Singapore</h1>
-                            <p className='text-[#1E1E1E]'>6 days trip</p>
+                            <p className='text-[#1E1E1E] mt-2'>6 days trip</p>
                         </div>
                         <div>
                             <p className='font-semibold'>$7.00K</p>
