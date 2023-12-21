e794f31c194e94e34d33dd1e20e994a2735600ec
ShrabonTambuli <shrabonsa@gmail.com>
2023-12-22 00:26:31 +0600

    update shop page

--- src/Pages/Shop/Shop.jsx
+++ src/Pages/Shop/Shop.jsx
@@ -11,7 +11,7 @@ const Shop = () => {
             <div className='grid grid-cols-1 md:grid-cols-2 justify-items-center items-center gap-5 mt-20 container mx-auto'>
                 <div>
                     <div className="relative">
-                        <img className='rounded-lg w-full  h-[600px] opacity-60' src="https://www.cuddlynest.com/blog/wp-content/uploads/2022/05/shopping-in-rome-via-dei-condotti.jpg" />
+                        <img className='rounded-lg w-full  h-[600px] hover:opacity-50' src="https://www.cuddlynest.com/blog/wp-content/uploads/2022/05/shopping-in-rome-via-dei-condotti.jpg" />
                         <div className='absolute top-64 left-32 lg:left-80 text-center'>
                             <h1 className='text-black text-4xl font-serif'>Rome, Italy</h1>
                         </div>
@@ -20,20 +20,20 @@ const Shop = () => {
                 <div>
                     <div className='flex justify-center items-center'>
                         <div className="relative">
-                            <img className='rounded-lg w-[400px] mx-auto h-80 opacity-60 pe-3' src="https://www.gpsmycity.com/img/art_item/b8303cbb0ff-ceramics.jpg" />
+                            <img className='rounded-lg w-[400px] mx-auto h-80 hover:opacity-50 pe-3' src="https://www.gpsmycity.com/img/art_item/b8303cbb0ff-ceramics.jpg" />
                             <div className='absolute top-28 left-2 lg:left-28 text-center'>
                                 <h1 className='text-black text-4xl font-serif'>Rome, Italy</h1>
                             </div>
                         </div>
                         <div className="relative">
-                            <img className='rounded-lg w-[400px] mx-auto h-80 opacity-60 ps-3' src="https://www.telegraph.co.uk/content/dam/Travel/2023/rome-shopping-GettyImages-160589015.jpg" />
+                            <img className='rounded-lg w-[400px] mx-auto h-80 hover:opacity-50 ps-3' src="https://www.telegraph.co.uk/content/dam/Travel/2023/rome-shopping-GettyImages-160589015.jpg" />
                             <div className='absolute top-28 left-5 lg:left-28 text-center'>
                                 <h1 className='text-black text-4xl font-serif'>Rome, Italy</h1>
                             </div>
                         </div>
                     </div>
                     <div className="relative mt-5">
-                        <img className='rounded-lg w-[768px] mx-auto h-[260px] opacity-60' src="https://theromanguy.com/italy-travel-blog/wp-content/uploads/How-Many-Days-Should-You-Spend-in-Rome-feature-1440-675.jpg" />
+                        <img className='rounded-lg w-[768px] mx-auto h-[260px] hover:opacity-50' src="https://theromanguy.com/italy-travel-blog/wp-content/uploads/How-Many-Days-Should-You-Spend-in-Rome-feature-1440-675.jpg" />
                         <div className='absolute top-28 left-32 lg:left-80 text-center'>
                             <h1 className='text-black text-4xl font-serif'>Rome, Italy</h1>
                         </div>
@@ -44,34 +44,30 @@ const Shop = () => {
                 <div>
                     <div className='flex justify-center items-center'>
                         <div className="relative">
-                            <img className='rounded-lg w-[400px] mx-auto h-80 opacity-60 pe-3' src="/images/dubai.avif" />
-                            <div className='absolute top-28 left-10 lg:left-36 text-center'>
-                                <h1 className='text-black text-4xl font-serif'>Dubai</h1>
-                                <h3 className='btn rounded-xl mt-6 text-white'>16 Tours</h3>
+                            <img className='rounded-lg w-[400px] mx-auto h-80 hover:opacity-50 pe-3' src="https://lp-cms-production.imgix.net/2023-08/GettyImages-1471449258.jpg" />
+                            <div className='absolute top-32 left-5 lg:left-28 text-center'>
+                                <h1 className='text-black text-4xl font-serif'>Singapore</h1>
                             </div>
                         </div>
                         <div className="relative">
-                            <img className='rounded-lg w-[400px] mx-auto h-80 opacity-60 ps-3' src="/images/Greece.jpg" />
-                            <div className='absolute top-28 left-10 lg:left-40 text-center'>
-                                <h1 className='text-black text-4xl font-serif'>Greece</h1>
-                                <h3 className='btn rounded-xl mt-6 text-white'>16 Tours</h3>
+                            <img className='rounded-lg w-[400px] mx-auto h-80 hover:opacity-50 ps-3' src="https://static.toiimg.com/photo/33025124.cms" />
+                            <div className='absolute top-32 left-7 lg:left-32 text-center'>
+                                <h1 className='text-black text-4xl font-serif'>Singapore</h1>
                             </div>
                         </div>
                     </div>
                     <div className="relative mt-5">
-                        <img className='rounded-lg w-[768px] mx-auto h-[260px] opacity-60' src="/images/Singapore.jpg" />
-                        <div className='absolute top-20 left-32 lg:left-80 text-center'>
+                        <img className='rounded-lg w-[768px] mx-auto h-[260px] hover:opacity-50' src="https://d27k8xmh3cuzik.cloudfront.net/wp-content/uploads/2017/10/OG23.jpg" />
+                        <div className='absolute top-28 left-32 lg:left-80 text-center'>
                             <h1 className='text-black text-4xl font-serif'>Singapore</h1>
-                            <h3 className='btn rounded-xl mt-6 text-white'>16 Tours</h3>
                         </div>
                     </div>
                 </div>
                 <div>
                     <div className="relative">
-                        <img className='rounded-lg w-full  h-[600px] opacity-60' src="/images/malaysia.jpg" />
+                        <img className='rounded-lg w-full  h-[600px] hover:opacity-50' src="https://a.cdn-hotels.com/gdcs/production172/d19/72229195-ddf7-47bc-bd75-21d3f95816fa.jpg?impolicy=fcrop&w=800&h=533&q=medium" />
                         <div className='absolute top-64 left-32 lg:left-80 text-center'>
-                            <h1 className='text-black text-4xl font-serif'>Malaysia</h1>
-                            <h3 className='btn rounded-xl mt-6 text-white'>16 Tours</h3>
+                            <h1 className='text-black text-4xl font-serif'>Singapore</h1>
                         </div>
                     </div>
                 </div>
@@ -79,35 +75,31 @@ const Shop = () => {
             <div className='grid grid-cols-1 md:grid-cols-2 justify-items-center items-center gap-5 mt-5 container mx-auto'>
                 <div>
                     <div className="relative">
-                        <img className='rounded-lg w-full  h-[600px] opacity-60' src="/images/malaysia.jpg" />
-                        <div className='absolute top-64 left-32 lg:left-80 text-center'>
-                            <h1 className='text-black text-4xl font-serif'>Malaysia</h1>
-                            <h3 className='btn rounded-xl mt-6 text-white'>16 Tours</h3>
+                        <img className='rounded-lg w-full  h-[600px] hover:opacity-50' src="https://greecetravelideas.com/wp-content/uploads/2020/12/Typical-athens-touristyc-jewelry-store-Greece-min-800x534.jpg" />
+                        <div className='absolute top-64 left-40 lg:left-80 text-center'>
+                            <h1 className='text-black text-4xl font-serif'>Greece</h1>
                         </div>
                     </div>
                 </div>
                 <div>
                     <div className='flex justify-center items-center'>
                         <div className="relative">
-                            <img className='rounded-lg w-[400px] mx-auto h-80 opacity-60 pe-3' src="/images/dubai.avif" />
-                            <div className='absolute top-28 left-10 lg:left-36 text-center'>
-                                <h1 className='text-black text-4xl font-serif'>Dubai</h1>
-                                <h3 className='btn rounded-xl mt-6 text-white'>16 Tours</h3>
+                            <img className='rounded-lg w-[400px] mx-auto h-80 hover:opacity-50 pe-3' src="https://images.squarespace-cdn.com/content/v1/5a07137264b05f2730b14c6b/1520505230895-IVKCFRD84L9DS33L2JTT/mati_talisman.jpg" />
+                            <div className='absolute top-32 left-10 lg:left-36 text-center'>
+                                <h1 className='text-black text-4xl font-serif'>Greece</h1>
                             </div>
                         </div>
                         <div className="relative">
-                            <img className='rounded-lg w-[400px] mx-auto h-80 opacity-60 ps-3' src="/images/Greece.jpg" />
-                            <div className='absolute top-28 left-10 lg:left-40 text-center'>
+                            <img className='rounded-lg w-[400px] mx-auto h-80 hover:opacity-50 ps-3' src="https://thumbs.dreamstime.com/b/shop-tourist-souvenir-street-fira-town-santorini-island-greece-santorini-greece-september-shop-tourist-203628511.jpg" />
+                            <div className='absolute top-32 left-14 lg:left-40 text-center'>
                                 <h1 className='text-black text-4xl font-serif'>Greece</h1>
-                                <h3 className='btn rounded-xl mt-6 text-white'>16 Tours</h3>
                             </div>
                         </div>
                     </div>
                     <div className="relative mt-5">
-                        <img className='rounded-lg w-[768px] mx-auto h-[260px] opacity-60' src="/images/Singapore.jpg" />
-                        <div className='absolute top-20 left-32 lg:left-80 text-center'>
-                            <h1 className='text-black text-4xl font-serif'>Singapore</h1>
-                            <h3 className='btn rounded-xl mt-6 text-white'>16 Tours</h3>
+                        <img className='rounded-lg w-[768px] mx-auto h-[260px] hover:opacity-50' src="https://img.freepik.com/premium-photo/souvenirs-souvenir-shop-crete-greece-traditional-greek-souvenirs-souvenir-shop-soft_265030-802.jpg?w=2000" />
+                        <div className='absolute top-28 left-40 lg:left-80 text-center'>
+                            <h1 className='text-black text-4xl font-serif'>Greece</h1>
                         </div>
                     </div>
                 </div>
